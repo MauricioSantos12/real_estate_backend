9377eab1b7f32ea3a259dbcb9fd8d00101208831
MauricioSantos12 <mauricio.santosr12@gmail.com>
2025-10-16 20:13:20 -0500

    temporal transport to send email

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,13 +1,15 @@
 const { emailSchema } = require("../schemas/emailsSchema");
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // true for 465, false for other ports
+let transporter = nodemailer.createTransport({
+  service: "gmail",
   auth: {
-    user: "maddison53@ethereal.email",
-    pass: "jn7jnAPss4f63QBp6D",
+    type: "OAuth2",
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
   },
 });
 
