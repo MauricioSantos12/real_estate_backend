f6195cff7c6a2e83ccfbe386d6787700bb8d78cd
MauricioSantos12 <mauricio.santosr12@gmail.com>
2025-10-22 16:25:48 -0500

    refactor: add config knexfile to production

--- knexfile.js
+++ knexfile.js
@@ -10,11 +10,19 @@ module.exports = {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
     },
-    migrations: {
-      directory: "./migrations",
+    migrations: { directory: "./migrations" },
+    seeds: { directory: "./seeds" },
+  },
+
+  production: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
-    seeds: {
-      directory: "./seeds",
-    },
+    migrations: { directory: "./migrations" },
+    seeds: { directory: "./seeds" },
   },
 };
